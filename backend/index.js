f6f943eb334fac31829f868bcb1b5d1da9fbfb24
Mafia_2404 <93379977+Mafia2404@users.noreply.github.com>
2025-06-27 11:21:06 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
- origin:["https://shopai-frontend.onrender.com" , "http://localhost:5174"],
+ origin:["https://shopai-frontend.onrender.com" , "https://shopai-admin.onrender.com"],
  credentials:true
 }))
 
